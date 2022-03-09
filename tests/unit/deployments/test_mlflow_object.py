493138f99499ed31312e894964b0ee66df2efc4f
Rafael Chaves <rafaelvchaves@gmail.com>
2022-03-08 19:10:31 -0500

    Fixed unit tests for model object

--- tests/unit/deployments/test_mlflow_object.py
+++ tests/unit/deployments/test_mlflow_object.py
@@ -4,23 +4,22 @@ import numpy as np
 import pandas as pd
 
 columns = ["fixed acidity", "volatile acidity", "citric acid", "residual sugar",
-            "chlorides", "free sulfur dioxide", "total sulfur dioxide", "density",
-            "pH", "sulphates", "alcohol"]
+           "chlorides", "free sulfur dioxide", "total sulfur dioxide", "density",
+           "pH", "sulphates", "alcohol"]
 data = [[6.2, 0.66, 0.48, 1.2, 0.029, 29, 75, 0.98, 3.33, 0.39, 12.8]]
 df = pd.DataFrame(columns=columns, data=data)
+
 
 def test_sklearn_regression_predict() -> None:
     model = mlflowobject.MLFlowModelObject()
     model.load_context("s3://coml-mlflow-models/sklearn-regression-model")
     assert np.allclose(model.predict(df), [5.72476731])
 
-def test_sklearn_regression_no_ctx() -> None:
-  model = mlflowobject.MLFlowModelObject()
-  try:
-    model.predict(df)
-    assert False
-  except CoMLException:
-    pass
 
-test_sklearn_regression_predict()
-test_sklearn_regression_no_ctx()
+def test_sklearn_regression_no_ctx() -> None:
+    model = mlflowobject.MLFlowModelObject()
+    try:
+        model.predict(df)
+        assert False
+    except CoMLException:
+        pass
