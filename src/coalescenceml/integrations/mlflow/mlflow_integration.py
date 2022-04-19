1e99064731d9f02020f697d97e215ed61f21bfcd
Iris Li <icl5@cornell.edu>
2022-04-19 18:30:20 -0400

    mlflow integration

--- src/coalescenceml/integrations/mlflow/mlflow_integration.py
+++ src/coalescenceml/integrations/mlflow/mlflow_integration.py
@@ -3,4 +3,4 @@ from coalescenceml.integrations.integration import Integration
 class MLFlowIntegration(Integration):
     NAME = "mlflow_integration"
     REQUIREMENTS = ["mlflow==1.25"]
-    SYSTEM_REQUIREMENTS = { "docker-engine" : "docker", "kubectl" : "kubectl" }
+    SYSTEM_REQUIREMENTS = { "docker-engine" : "docker", "kubectl" : "kubectl" , "conda" : "conda"}
