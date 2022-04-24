a5b9e8258942c657810f32b37e6cf28554e109eb
Magd Bayoumi <mb2363@cornell.edu>
2022-04-24 10:19:02 -0400

    misc: Added acorns and status spinner for hidden command

--- src/coalescenceml/cli/hidden.py
+++ src/coalescenceml/cli/hidden.py
@@ -1,3 +1,5 @@
+import time
+
 import click
 
 from coalescenceml.cli.cli import cli
@@ -8,25 +10,31 @@ from coalescenceml.logger import get_logger
 
 @cli.command(name="over", help="Funny joke...hehe", hidden=True)
 def print_deez_nutz_joke()->None:
-    console.print("coml over...", style="magenta", justify="center")
-    deez_nutz = """
- _____    ______   ______   ______    _   _   _    _   _______   ______
-|  __ \  |  ____| |  ____| |___  /   | \ | | | |  | | |__   __| |___  /
-| |  | | | |__    | |__       / /    |  \| | | |  | |    | |       / /
-| |  | | |  __|   |  __|     / /     | . ` | | |  | |    | |      / /
-| |__| | | |____  | |____   / /__    | |\  | | |__| |    | |     / /__
-|_____/  |______| |______| /_____|   |_| \_|  \____/     |_|    /_____|"""
+    deez_nutz = r"""
+      -                                                                                    -
+    _/-\_                                                                                _/-\_
+ .-`-:-:-`-.   _____    ______   ______   ______    _   _   _    _   _______   ______  .-`-:-:-`-.
+/-:-:-:-:-:-\ |  __ \  |  ____| |  ____| |___  /   | \ | | | |  | | |__   __| |___  / /-:-:-:-:-:-\
+\:-:-:-:-:-:/ | |  | | | |__    | |__       / /    |  \| | | |  | |    | |       / /  \:-:-:-:-:-:/
+ |`       `|  | |  | | |  __|   |  __|     / /     | . ` | | |  | |    | |      / /    |`       `|
+ |         |  | |__| | | |____  | |____   / /__    | |\  | | |__| |    | |     / /__   |         |
+ `\       /`  |_____/  |______| |______| /_____|   |_| \_|  \____/     |_|    /_____|  `\       /`
+   `-._.-'                                                                               `-._.-'"""
 
-    console.rule("")
     max_width = console.width
     deez_nutz_width = console.measure(deez_nutz).maximum
-    if deez_nutz_width > max_width:
-        console.print(deez_nutz, style="bold magenta")
+    styling = "bold cyan"
+    console.print("coml over...", style="cyan", justify="center")
+    with console.status("", spinner="monkey"):
+        time.sleep(5)
+
+    if deez_nutz_width >= max_width:
+        console.print(deez_nutz, style=styling, overflow="crop")
         console.rule(f"")
     else:
         width_buffer = int((max_width - deez_nutz_width) / (2))
         deez_nutz = "\n".join([
             ((" "* width_buffer) + line) for line in deez_nutz.split("\n")
         ])
-        console.print(deez_nutz, style="bold magenta")
+        console.print(deez_nutz, style=styling)
         console.rule("")
