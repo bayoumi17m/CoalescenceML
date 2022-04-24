894409dc46481535e6aa701d63f4344fe96714bb
Rafael Chaves <rafaelvchaves@gmail.com>
2022-04-24 16:44:43 -0400

    Changed deploy step to return json rather than string

--- src/coalescenceml/integrations/mlflow/step/kubedeployer.py
+++ src/coalescenceml/integrations/mlflow/step/kubedeployer.py
@@ -1,27 +1,22 @@
-from deployments.base_deploy_step import BaseDeploymentStep
+from coalescenceml.logger import get_logger
+from coalescenceml.model_deployments.base_deploy_step import BaseDeploymentStep
 from config import DeploymentYAMLConfig
 import json
 import subprocess
+from typing import Any, Dict
+
+logger = get_logger(__name__)
 
 
 class KubernetesDeployer(BaseDeploymentStep):
     """Step class for deploying model to Kubernetes."""
 
-    def __del__(self):
-        self.deploy_log.close()
-
-    def run_cmd(self, cmd, start_msg, err_msg):
-        print(start_msg)
-        proc = subprocess.run(cmd, stdout=subprocess.PIPE,
-                              stderr=subprocess.STDOUT, text=True)
-        self.deploy_log.write(proc.stdout)
-        if not self.suppress_stdout:
-            print(proc.stdout)
+    def run_cmd(self, cmd):
+        logger.debug(f"Executing command: {' '.join(cmd)}")
+        proc = subprocess.run(cmd, text=True)
         if proc.returncode != 0:
-            print("Error: {}. See log for more info.".format(err_msg))
-            self.deploy_log.write("Failed command: {}\n".format(proc.args))
+            logger.error(f"Command failed: {proc.stderr}")
             exit(1)
-        print("done")
 
     # Change to custom Flask app instead of mlflow build-docker
     def build_model_image(self):
@@ -32,78 +27,53 @@ class KubernetesDeployer(BaseDeploymentStep):
         """
         build_cmd = ["mlflow", "models", "build-docker",
                      "-m", self.model_uri, "-n", self.registry_path]
-        self.run_cmd(build_cmd, "Building docker image...",
-                     "Failed to build docker image")
+        self.run_cmd(build_cmd)
 
     # Not sure if this step is actually needed
     def push_image(self):
         """Pushes the docker image to the provided registry path."""
-        self.run_cmd(
-            ["docker", "push", self.registry_path],
-            "Pushing image to registry...",
-            "Failed to push image"
-        )
+        self.run_cmd(["docker", "push", self.registry_path])
 
     def config_deployment(self):
         """Configures the deployment.yaml and service.yaml files for deployment."""
-        print("Configuring deployment...", end="")
         config = DeploymentYAMLConfig(self.deployment_name, self.registry_path)
         config.create_deployment_yaml()
         config.create_service_yaml()
-        print("done")
 
     def deploy(self):
         """Applies the deployment and service yamls."""
         deploy_cmd = ["kubectl", "apply", "-f", "deployment.yaml"]
         service_cmd = ["kubectl", "apply", "-f", "service.yaml"]
-        self.run_cmd(deploy_cmd, "Creating deployment...",
-                     "Failed to create deployment")
-        self.run_cmd(service_cmd, "Creating LoadBalancer service...",
-                     "Failed to create LoadBalancer service")
+        self.run_cmd(deploy_cmd)
+        self.run_cmd(service_cmd)
 
-    def get_service_url(self):
-        """Get service URL.
-
-        Searches kubectl for the service name and returns its external IP,
-        if any.
-        """
+    def get_deployment_info(self) -> Dict[str, Any]:
         p = subprocess.run(["kubectl", "get", "service",
                            self.service_name, "--output=json"], capture_output=True)
-        service_json = json.loads(p.stdout)
-        ip = service_json["status"]["loadBalancer"]["ingress"][0]["ip"]
-        if ip is None or ip == "":
-            print("Error: could not find IP")
-            return None
-        elif ip == "<pending>":
-            print("Ingress IP is currently pending, please try again later.")
-        return "http://{}".format(ip)
+        return json.loads(p.stdout)
 
-    def entrypoint(self, model_uri, registry_path, deploy, suppress_stdout=True):
+    def entrypoint(self, model_uri: str, registry_path: str, deploy: bool) -> Dict[str, Any]:
         if not deploy:
             return None
         self.model_uri = model_uri
         self.registry_path = registry_path
         self.deployment_name = "mlflow-deployment"
-        self.service_name = self.deployment_name + "-service"
-        self.suppress_stdout = suppress_stdout
-        self.deploy_log = open("kubedeploy.log", "w")
+        self.service_name = "mlflow-deployment-service"
         self.build_model_image()
         self.push_image()
         self.config_deployment()
         self.deploy()
-        url = self.get_service_url()
-        return url
+        deployment_info = self.get_deployment_info()
+        return deployment_info
 
 
 kd = KubernetesDeployer()
 
-project_id = "mlflow-gcp-testing"
-repo = "mlflow-repo"
 
-url = kd.entry_point(
+deployment_info = kd.entrypoint(
     "s3://coml-mlflow-models/sklearn-regression-model",
-    "k8s.gcr.io/sklearnmodel",
-    deploy=True,
-    suppress_stdout=False)
+    "us-east1-docker.pkg.dev/mlflow-gcp-testing/mlflow-repo/sklearn-model",
+    deploy=True
+)
 
-print("URL: ", url)
+print(deployment_info)
--- src/coalescenceml/model_deployments/base_deploy_step.py
+++ src/coalescenceml/model_deployments/base_deploy_step.py
@@ -1,14 +1,13 @@
-from step.base_step import BaseStep
-from abc import ABC, abstractmethod
-from modelobject import ModelObject
-from typing import Optional
+from coalescenceml.step.base_step import BaseStep
+from abc import abstractmethod
+from typing import Any, Dict
 
 
 class BaseDeploymentStep(BaseStep):
     """Base class for a deployment step in a CoalescenceML pipeline. """
 
     @abstractmethod
-    def entrypoint(self, model_uri: str, registry_path: str, deploy: bool) -> Optional[str]:
+    def entrypoint(self, model_uri: str, registry_path: str, deploy: bool) -> Dict[str, Any]:
         """Deploys model specified by uri, if deploy is True.
 
         The model is built into an image at the given path, pushed
@@ -16,6 +15,6 @@ class BaseDeploymentStep(BaseStep):
         in the kubeconfig file.
 
         Returns:
-          A url for the deployment, if successful, otherwise None.
+          A dictionary containing deployment info.
         """
         pass
