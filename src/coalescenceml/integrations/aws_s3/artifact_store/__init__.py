fbfbf36d5d506c32f3a4222c7af4b8720e70fc98
Iris Li <icl5@cornell.edu>
2022-05-08 14:29:43 -0400

    fixed typo in import path

--- src/coalescenceml/integrations/aws_s3/artifact_store/__init__.py
+++ src/coalescenceml/integrations/aws_s3/artifact_store/__init__.py
@@ -1,3 +1,3 @@
-from coalescenceml.integrations.aws_s3.artifact_store.azure_artifact_store import (
+from coalescenceml.integrations.aws_s3.artifact_store.aws_artifact_store import (
     AWSArtifactStore,
 ) 
